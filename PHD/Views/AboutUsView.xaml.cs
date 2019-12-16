e00dc127370a1dcb20759c47536f66a41214d4ec
flashofsilver <grindwood@gmail.com>
2019-12-16 18:30:03 -0500

    Updated About Us Page

---
+++ PHD/Views/AboutUsView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace PHD.Views
+{
+    public partial class AboutUsView : ContentPage
+    {
+        public AboutUsView()
+        {
+            InitializeComponent();
+        }
+    }
+}
